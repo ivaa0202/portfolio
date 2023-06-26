17059095444e176af4f1ad15321de1beeed24f17
ivaa0202 <ivaa.0202@gmail.com>
2023-06-26 15:29:29 +0200

    project done

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='introduction'>
+        <div>
+            <img src="../../..//photo_5988040402357304483_y.jpg" alt="" />
+        </div>
+        <div>
+            <p>Hello!</p>
+            <p>I am Iva Lukač</p>
+            <p>Frontend Developer</p>
+            <a href="">Resume</a>
+        </div>
+    </div>
+  )
+}
---
+++ src/Projects.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function Projects() {
+  return (
+    <div class="projects">
+    <h1>Latest Work</h1>
+    <div class="one-project">
+        <div class="picture">
+            <img src="../../../FrgFwsbWYAMuz5R.jpg_large" alt="" />
+        </div>
+        <div class="description">
+            <h2>F1</h2>
+            <a href="https://ivaa0202.github.io/F1/f1">Full project</a>
+        </div>
+    </div>
+
+    <div class="one-project">
+        <div class="description">
+            <h2>Toptal</h2>
+            <a href="https://ivaa0202.github.io/toptal">Full project</a>
+        </div>
+        <div class="picture">
+            <img src="../../../profile-img copy.webp" alt="" />
+        </div>
+    </div>
+
+    <div class="one-project">
+        <div class="picture">
+            <img src="../../../SaudiTourism-2019101702130178.png" alt="" />
+        </div>
+        <div class="description">
+            <h2>Visit Saudi Arabia</h2>
+            <a href="https://ivaa0202.github.io/visit-saudi-arabia">Full project</a>
+        </div>
+    </div>
+</div>
+  )
+}
